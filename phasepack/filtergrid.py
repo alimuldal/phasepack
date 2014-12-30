cd669ab63b1d05d5af66e8cef7da943ee26b99e2
Alistair Muldal <alimuldal@gmail.com>
2014-12-30 19:59:53 +0000

    cleaner code for generating u1 & u2 matrices

--- phasepack/filtergrid.py
+++ phasepack/filtergrid.py
@@ -3,23 +3,17 @@ from scipy.fftpack import ifftshift
 
 
 def filtergrid(rows, cols):
-	# Set up u1 and u2 matrices with ranges normalised to +/- 0.5
-	if (cols % 2):
-		xvals = np.arange(-(cols-1)/2., ((cols-1)/2.)+1) / float(cols-1)
-	else:
-		xvals = np.arange(-cols/2., cols/2.) / float(cols)
 
-	if (rows % 2):
-		yvals = np.arange(-(rows-1)/2., ((rows-1)/2.)+1) / float(rows-1)
-	else:
-		yvals = np.arange(-rows/2., rows/2.) / float(rows)
-	u1,u2 = np.meshgrid(xvals,yvals,sparse=True)
+    # Set up u1 and u2 matrices with ranges normalised to +/- 0.5
+    u1, u2 = np.meshgrid(np.linspace(-0.5, 0.5, cols, endpoint=(cols % 2)),
+                         np.linspace(-0.5, 0.5, rows, endpoint=(rows % 2)),
+                         sparse=True)
 
-	# Quadrant shift to put 0 frequency at the corners
-	u1 = ifftshift(u1)
-	u2 = ifftshift(u2)
+    # Quadrant shift to put 0 frequency at the top left corner
+    u1 = ifftshift(u1)
+    u2 = ifftshift(u2)
 
-	# Compute frequency values as a radius from centre (but quadrant shifted)
-	radius = np.sqrt(u1*u1 + u2*u2)
+    # Compute frequency values as a radius from centre (but quadrant shifted)
+    radius = np.sqrt(u1*u1 + u2*u2)
 
-	return radius, u1, u2
+    return radius, u1, u2

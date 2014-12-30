c64a8fed729c80c65d6b9c5d1bf9e7ce5326dcd5
Alistair Muldal <alimuldal@gmail.com>
2014-12-30 20:55:27 +0000

    add github URL to setup.py, incremented version to 1.4, uploaded new source to PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(	name = 'phasepack',
-	version = '1.3',
-	description = 'A toolkit for phase-based feature detection',
-	author = 'Alistair Muldal',
-	author_email = 'alistair.muldal@pharm.ox.ac.uk',
-	packages = ['phasepack'])
-
+setup(name='phasepack',
+      version='1.4',
+      description='A toolkit for phase-based feature detection',
+      author='Alistair Muldal',
+      author_email='alistair.muldal@pharm.ox.ac.uk',
+      url='https://github.com/alimuldal/phasepack',
+      packages=['phasepack'])
